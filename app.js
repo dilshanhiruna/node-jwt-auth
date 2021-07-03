b4f420e763f6478158a3d6555623b6e52630b9fd
dilshanhiruna <dilshan.hirunaa@gmail.com>
2021-07-03 17:41:00 +0530

    user model created

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
 // middleware
 app.use(express.static("public"));
+app.use(express.json());
 
 // view engine
 app.set("view engine", "ejs");
@@ -30,3 +31,5 @@ connection.once("open", () => {
 // routes
 app.get("/", (req, res) => res.render("home"));
 app.get("/smoothies", (req, res) => res.render("smoothies"));
+
+app.use(authRoutes);
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,27 @@
+const User = require("../models/Users");
+
 module.exports.signup_get = (req, res) => {
   res.render("signup");
 };
 module.exports.login_get = (req, res) => {
   res.render("login");
 };
-module.exports.signup_post = (req, res) => {
-  req.send("new Signup");
+module.exports.signup_post = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.create({
+      email,
+      password,
+    });
+    res.status(201).json(user);
+  } catch (err) {
+    console.log(err);
+    res.status(400).send("error, user not created");
+  }
 };
-module.exports.login_post = (req, res) => {
-  req.send("new Login");
+module.exports.login_post = async (req, res) => {
+  const { email, password } = req.body;
+  console.log(email, password);
+  res.send("new Login");
 };
