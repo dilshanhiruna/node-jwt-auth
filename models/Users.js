d0ee2ee7fc58b0050d4e6f9e9a2dc89bb91a02e7
dilshanhiruna <dilshan.hirunaa@gmail.com>
2021-07-03 18:30:04 +0530

    implememted mongoose validator

--- models/Users.js
+++ models/Users.js
@@ -1,16 +1,18 @@
 const mongoose = require("mongoose");
+const { isEmail } = require("validator");
 
 const UserSchema = new mongoose.Schema({
   email: {
     type: String,
-    required: true,
+    required: [true, "please enter an email"],
     unique: true,
     lowercase: true,
+    validate: [isEmail, "Please enter a valid email"],
   },
   password: {
     type: String,
-    required: true,
-    minLength: 6,
+    required: [true, "please enter an pasword"],
+    minlength: [6, "Minimun pass length is 6"],
   },
 });
 
