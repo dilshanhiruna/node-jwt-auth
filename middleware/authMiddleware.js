6e4d5dd2067fc59b6fa6ad348f90b135eec46547
dilshanhiruna <dilshan.hirunaa@gmail.com>
2021-07-04 11:40:49 +0530

    check user funtion

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -25,16 +25,23 @@ const checkUser = (res, req, next) => {
   const token = req.cookies.jwt;
 
   if (token) {
-    jwt.verify(token, process.env.JWT, (err, decodedToken) => {
+    jwt.verify(token, process.env.JWT, async (err, decodedToken) => {
       if (err) {
         console.log(err.message);
+        res.locals.user = null;
         next();
       } else {
         console.log(decodedToken);
+
+        let user = await User.findById(decodedToken.id);
+        res.locals.user = user;
+
         next();
       }
     });
   } else {
+    res.locals.user = null;
+    next();
   }
 };
 
